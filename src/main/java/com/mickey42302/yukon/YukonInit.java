199057813244a6769b599eccf8c1907d6a750eea
Mickey42302 <46900084+Mickey42302@users.noreply.github.com>
2025-12-18 19:23:53 -0500

    Add files via upload

--- src/main/java/com/mickey42302/yukon/YukonInit.java
+++ src/main/java/com/mickey42302/yukon/YukonInit.java
@@ -5,22 +5,15 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static net.fabricmc.loader.impl.FabricLoaderImpl.MOD_ID;
 
 public class YukonInit implements ModInitializer {
-
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
 
-        LOGGER.info("Loading Yukon...");
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.OPERATOR).register(content -> content.add(Items.PETRIFIED_OAK_SLAB));
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.OPERATOR).register(content -> {
-            content.add(Items.PETRIFIED_OAK_SLAB);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
             content.add(Items.ENDER_DRAGON_SPAWN_EGG);
             content.add(Items.WITHER_SPAWN_EGG);
         });
