94119ab7f8032563b2e4b076ff9c8dd9783416ed
Mickey42302 <46900084+Mickey42302@users.noreply.github.com>
2026-06-25 18:17:27 -0400

    Add files via upload

--- src/client/java/com/mickey42302/yukon/util/LastServerUtil.java
+++ src/client/java/com/mickey42302/yukon/util/LastServerUtil.java
@@ -1,28 +1,29 @@
 package com.mickey42302.yukon.util;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
-import net.minecraft.client.network.ServerAddress;
-import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.ConnectScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
+import net.minecraft.client.multiplayer.ServerData;
 
-public enum LastServerUtil
-{
+public enum LastServerUtil {
 	;
-	
-	private static ServerInfo lastServer;
-	
-	public static void setLastServer(ServerInfo server)
+
+	private static ServerData lastServer;
+
+	public static void setLastServer(ServerData server)
 	{
 		lastServer = server;
 	}
 
 	public static void reconnect(Screen prevScreen)
 	{
-		if(lastServer == null)
+		if(lastServer == null) {
+			System.out.println("Cannot reconnect: lastServer is null.");
 			return;
-		
-		ConnectScreen.connect(prevScreen, MinecraftClient.getInstance(),
-			ServerAddress.parse(lastServer.address), lastServer, false, null);
+		}
+
+		ConnectScreen.startConnecting(prevScreen, Minecraft.getInstance(),
+				ServerAddress.parseString(lastServer.ip), lastServer, false, null);
 	}
 }
